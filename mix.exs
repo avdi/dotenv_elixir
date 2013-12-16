9fef0927d1f1e00ecfe9bd5ae415d7a663d02aea
Avdi Grimm <avdi@avdi.org>
2013-12-16 02:03:19 -0500

    Update Elixir version requirement.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule DotenvElixir.Mixfile do
   def project do
     [ app: :dotenv_elixir,
       version: "0.0.1",
-      elixir: "~> 0.11.1",
+      elixir: "~> 0.11",
       deps: deps ]
   end
 
