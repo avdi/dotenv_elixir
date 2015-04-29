d2a15098d2811f0c9f714773d6b47666500173ed
Jared Norman <jared@freerunningtechnologies.com>
2015-04-28 22:31:29 -0700

    Bump v1.0.0
    Change-Id: I69fafde5bd5a1c241af8c903940e9ca4c672debc

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule DotenvElixir.Mixfile do
 
   def project do
     [ app: :dotenv,
-      version: "0.0.4",
+      version: "1.0.0",
       elixir: "~> 1.0.0",
       deps: deps,
       package: [
