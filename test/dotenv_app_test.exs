02bb885f0d3442d19d62ed59e94e5cf436533515
Gal Tsubery <gal.tsubery@gmail.com>
2017-02-17 17:07:27 -0600

    Fix Elixir 1.4 warnings

--- test/dotenv_app_test.exs
+++ test/dotenv_app_test.exs
@@ -2,11 +2,11 @@ defmodule DotenvAppTest do
   use ExUnit.Case
 
   def fixture_dir, do: Path.expand("../fixture", __ENV__.file())
-  def proj1_dir, do: Path.join(fixture_dir, "proj1")
+  def proj1_dir, do: Path.join(fixture_dir(), "proj1")
   def root_dir, do: Path.expand("../..", __ENV__.file())
 
   setup do
-    Dotenv.reload!(Path.join(root_dir, ".env"))
+    Dotenv.reload!(Path.join(root_dir(), ".env"))
     on_exit fn ->
       System.put_env "APP_TEST_VAR", ""
       System.put_env "FOO_BAR", ""
@@ -15,14 +15,14 @@ defmodule DotenvAppTest do
   end
 
   test "reloading from a new file" do
-    Dotenv.reload!(Path.join(proj1_dir, ".env"))
+    Dotenv.reload!(Path.join(proj1_dir(), ".env"))
     assert Dotenv.get("FOO_BAR") == "1234"
     assert System.get_env("FOO_BAR") == "1234"
   end
 
   test "fetching the whole environment" do
     env = Dotenv.env
-    assert Dict.get(env.values, "APP_TEST_VAR") == "HELLO"
+    assert Map.get(env.values, "APP_TEST_VAR") == "HELLO"
   end
 
   test "getting a value with a fallback" do
