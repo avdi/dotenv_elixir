28f4836c206d682793f7d2a2c5b272ba3a286e1e
kajisha <kajisha@gmail.com>
2017-04-20 09:14:13 +0900

    Add parentheses

--- test/dotenv_app_test.exs
+++ test/dotenv_app_test.exs
@@ -2,11 +2,11 @@ defmodule DotenvAppTest do
   use ExUnit.Case
 
   def fixture_dir, do: Path.expand("../fixture", __ENV__.file())
-  def proj1_dir, do: Path.join(fixture_dir, "proj1")
+  def proj1_dir, do: Path.join(fixture_dir(), "proj1")
   def root_dir, do: Path.expand("../..", __ENV__.file())
 
   setup do
-    Dotenv.reload!(Path.join(root_dir, ".env"))
+    Dotenv.reload!(Path.join(root_dir(), ".env"))
     on_exit fn ->
       System.put_env "APP_TEST_VAR", ""
       System.put_env "FOO_BAR", ""
@@ -15,7 +15,7 @@ defmodule DotenvAppTest do
   end
 
   test "reloading from a new file" do
-    Dotenv.reload!(Path.join(proj1_dir, ".env"))
+    Dotenv.reload!(Path.join(proj1_dir(), ".env"))
     assert Dotenv.get("FOO_BAR") == "1234"
     assert System.get_env("FOO_BAR") == "1234"
   end
