11effeb0ff8519ddeb04dbbbe2e56f071f830fd0
Jared Norman <jared@creepywizard.com>
2017-07-23 08:37:10 -0700

    Bump to v3.0.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule DotenvElixir.Mixfile do
 
   def project do
     [ app: :dotenv,
-      version: "2.1.0",
+      version: "3.0.0",
       elixir: "~> 1.0",
       deps: deps(),
       package: [
