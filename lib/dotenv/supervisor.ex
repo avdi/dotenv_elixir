3f51dc0bd2326407b1c157c5cabac27022efa809
Michael Bianco <iloveitaly@gmail.com>
2020-12-21 07:40:28 -0700

    Fix deprecation warnings on 1.11

--- lib/dotenv/supervisor.ex
+++ lib/dotenv/supervisor.ex
@@ -3,14 +3,11 @@ defmodule Dotenv.Supervisor do
   use Supervisor
 
   def start_link(env_path \\ :automatic) do
-    :supervisor.start_link(__MODULE__, env_path)
+    Supervisor.start_link(__MODULE__, env_path)
   end
 
   def init(env_path) do
-    children = [worker(Dotenv.Server, env_path)]
-
-    # See http://elixir-lang.org/docs/stable/Supervisor.Behaviour.html
-    # for other strategies and supported options
-    supervise(children, strategy: :one_for_one)
+    children = [Supervisor.child_spec({Dotenv.Server, env_path}, [])]
+    Supervisor.init(children, strategy: :one_for_one)
   end
 end
